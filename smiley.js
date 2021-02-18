a6508abca0fcde1fde08a3cff34d0ae7de920f97
Aidana27 <75001581+Aidana27@users.noreply.github.com>
2021-02-18 23:18:28 +0600

    Add files via upload

---
+++ smiley.js
@@ -0,0 +1,20 @@
+function happy(){
+    let input = document.querySelector('.main-smiley')
+
+
+    input.innerHTML = '	&#128578;';
+}
+
+function normal(){
+    let input = document.querySelector('.main-smiley')
+
+
+    input.innerHTML = '&#128528;';
+}
+
+function angry(){
+    let input = document.querySelector('.main-smiley')
+
+
+    input.innerHTML = '&#128577;';
+} 
